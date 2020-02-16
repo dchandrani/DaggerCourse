8334718c1a2339e54e5c33f47e24f3c539772311
Dhruv Chandrani <dhruv.chandrani@gmail.com>
2020-02-16 19:05:29 +0530

    Injection using @Inject annotation

--- app/src/main/java/com/app/daggercourse/MainActivity.java
+++ app/src/main/java/com/app/daggercourse/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
+    @Inject
+    Car car;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CarComponent carComponent = DaggerCarComponent.create();
+        carComponent.inject(this);
+
+        car.drive();
     }
 }
---
+++ app/src/main/java/com/app/daggercourse/Wheels.java
@@ -0,0 +1,9 @@
+package com.app.daggercourse;
+
+import javax.inject.Inject;
+
+public class Wheels {
+    @Inject
+    public Wheels() {
+    }
+}
